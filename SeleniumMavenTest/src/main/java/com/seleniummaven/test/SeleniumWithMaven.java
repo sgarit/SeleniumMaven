df3646c5d9ddf53fde8088fd66ed84e14188a746
ragin <ragin@10.0.1.34>
2019-01-17 11:58:27 -0500

    Adding files to Selenium maven project

---
+++ SeleniumMavenTest/src/main/java/com/seleniummaven/test/SeleniumWithMaven.java
@@ -0,0 +1,22 @@
+package com.seleniummaven.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumWithMaven {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ragin\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		System.out.println(driver.getTitle());
+		driver.manage().window().maximize();
+		
+		}
+
+	
+
+}
