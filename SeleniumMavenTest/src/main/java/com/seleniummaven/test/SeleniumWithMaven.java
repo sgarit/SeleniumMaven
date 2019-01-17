fb8647c2998105fba0f1deb7f17dc565ee1bb143
Ragini N <sgar.it@outlook.com>
2019-01-17 12:08:05 -0500

    Update SeleniumWithMaven.java
    added some code to selenium maven class

--- SeleniumMavenTest/src/main/java/com/seleniummaven/test/SeleniumWithMaven.java
+++ SeleniumMavenTest/src/main/java/com/seleniummaven/test/SeleniumWithMaven.java
@@ -13,6 +13,7 @@ public class SeleniumWithMaven {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com");
 		System.out.println(driver.getTitle());
+		System.out.println("Ragini Nara");
 		driver.manage().window().maximize();
 		
 		}
